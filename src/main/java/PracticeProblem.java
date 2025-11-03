ec4af12258f081c2eaed637da2fbd6b6d9d45e90
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-03 18:10:20 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,61 @@
+import java.util.Arrays;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		
+
 	}
-	
+	public static int[] compareSearch(int[] array, int number) {
+		int seqLoops = 0;
+		int binLoops = 0;
+		for (int i = 0; i < array.length; i++) {
+			seqLoops++;
+			if (array[i] == number) {
+				break;
+			}
+		}
+		Arrays.sort(array);
+		int low = 0;
+		int high = array.length - 1;
+
+		while (low<= high) {
+			binLoops++;
+			int mid = (low + high) / 2;
+			if (array[mid] == number) {
+				break;
+			} else if (array[mid] < number) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return new int[] {seqLoops, binLoops};
+	}
+	public static int[]
+	compareStringSearch(String[] array, String word) {
+		int seqLoops = 0;
+		int binLoops = 0;
+		for (int i = 0; i < array.length; i++) {
+			seqLoops++;
+			if (array[i].equals(word)) {
+				break;
+			}
+		}
+		Arrays.sort(array);
+		int low = 0;
+		int high = array.length - 1;
+
+		while (low <= high) {
+			binLoops++;
+			int mid = (low + high) / 2;
+			int compare = array[mid].compareTo(word);
+			if (compare == 0) {
+				break;
+			} else if (compare < 0) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return new int[] {seqLoops, binLoops};
+	}
 }
